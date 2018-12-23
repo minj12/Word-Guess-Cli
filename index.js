c6edcadd36c5591f220b8f0e38603783d3a61106
min <mjnorthwesternwork1121@gmail.com>
2018-12-23 00:02:19 -0600

    nesting excisting functions

--- index.js
+++ index.js
@@ -15,11 +15,46 @@ function startGame() {
     if (wordList.length<2) {
         wordList = ["BAT MAN", "AQUA MAN", "HIT GIRL", "SPIDER GIRL", "SPIDER WOMAN", "THOR", "HE-MAN", "PLASTIC MAN", "STATIC SHOCK", "ELONGATED MAN", "SPIDER GWEN", "BLACK SCORPIAN", "DARE DEVIL", "JESSICA JONES"];
     }
-    
-function startGame() {}
+     select = Math.floor(Math.random()*wordList.length);
+    chosenWord = wordList[select];
+    gameWord = new Word(chosenWord);
+    gameWord.makeWord();
+    if (select > -1) {
+        wordList.splice(select, 1);
+    }
+    console.log("\nYou get 8 letter guesses to find the Mario character.\n".cyan)
+    promptUser();
+}
 
-function promptUser() {}
+
+//Prompt function that lets user to input a letter guess, restarts the game if player is out of wrong guesses.
+function promptUser() {
+    if (counter<8) {
+        console.log(gameWord.showWord());
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "letter",
+                message: "\nPick a letter and press enter. ".cyan
+            }
+        ]).then(function(data) {
+                checkAnswer(data);
+        });
+    }
+    else{
+        console.log("\nSorry, you're out of guesses.\n".inverse);
+        console.log(chosenWord.rainbow);
+        chosenWord = "";
+        gameWord = "";
+        select = 0;
+        counter = 0;
+        startGame();
+    }
+}
 
 function checkAnswer(data) {}
 
 function rightGuess() {}
+
+
+
